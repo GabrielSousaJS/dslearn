9fff6fda87ec3a1ca703268e0db25acc820ed242
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-01-25 23:29:52 -0300

    Resource

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,9 +13,13 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Lorem ipsum', TIMESTAMP WITH TIME ZONE '2023-01-25T18:00:00Z', 'false', 'https://learn.devsuperior.com/offers/66/resource/367/sections/1296?item=16117', '1');
+
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-01-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-01-20T03:00:00Z', '1');
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-02-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-02-20T03:00:00Z', '1');
 
-INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Lorem ipsum', TIMESTAMP WITH TIME ZONE '2023-01-25T18:00:00Z', 'false', 'https://learn.devsuperior.com/offers/66/resource/367/sections/1296?item=16117', '1');
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', '1', '1');
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Suporte a dúvidas', 'Forum de perguntas e respostas', 2, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', '2', '1');
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives técnicas', 'Tire suas dúvidas ao vivo', 3, 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', '0', '1');
