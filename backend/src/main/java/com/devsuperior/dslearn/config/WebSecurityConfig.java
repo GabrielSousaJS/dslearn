dee04a3e23e5b56caf8a0c90ef2fde973cf66452
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-01-30 16:10:25 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearn/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dslearn/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dslearn.components.config;
+package com.devsuperior.dslearn.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
