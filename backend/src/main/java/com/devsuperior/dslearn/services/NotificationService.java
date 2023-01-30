ad944169d55f580f3c9b35f8b9d1065469ba601d
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-01-30 15:27:09 -0300

    Custom content for current user

---
+++ backend/src/main/java/com/devsuperior/dslearn/services/NotificationService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslearn.services;
+
+import com.devsuperior.dslearn.dto.NotificationDTO;
+import com.devsuperior.dslearn.entities.Notification;
+import com.devsuperior.dslearn.entities.User;
+import com.devsuperior.dslearn.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository repository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional(readOnly = true)
+    public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+        User user = authService.autheticated();
+        Page<Notification> page = repository.findByUser(user, pageable);
+        return page.map(NotificationDTO::new);
+    }
+}
